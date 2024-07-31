8e63999fb50c0310a2095dc38bd3629f87bb860b
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-07-31 07:24:03 +0200

    update the backend api

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: `https://mike-token-backend-1.onrender.com/api`,
+    // baseURL: `https://mike-token-backend-1.onrender.com/api`,
+    baseURL: 'https://erne-legacy-telegram-app-backend.onrender.com',
     headers: {
       'Content-Type': 'application/json'
     }
